f9a7b025100375f82877592770db57978760527c
Bartlomiej Calko <bartlomiejcalko@brainZero.local>
2025-01-08 17:16:45 +0100

    Configured eslint and prettier

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from "react";
 
 const Home = () => {
   return (
-    <h1 className='text-3xl font-bold text-violet-500'>Welcome to SkyOverflow</h1>
+    <h1 className="text-3xl font-bold text-white">
+      Welcome to SkyOverflow
+    </h1>
   );
-}
+};
 
 export default Home;
